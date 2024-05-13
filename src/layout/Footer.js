0e69a92b75288d4c0feb35f32871a2a4b40ebb5a
ymink716 <ymink716@gmail.com>
2024-05-13 19:57:53 +0900

    feat: header layout에 조건부 렌더링 적용 (로그인, 로그아웃)

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -7,6 +7,9 @@ function Footer() {
     window.location.replace('/');
   }
   
+  // 조건부 렌더링
+  // 로그인 안했을 시: 홈
+  // 로그인 했을 시: 새 글 작성, 홈, 프로필 
   return (
     <footer className='footer'>
       <div className='footer_center'>
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
-import { RiLoginBoxLine } from "react-icons/ri";
+import { RiLoginBoxLine, RiLogoutBoxLine } from "react-icons/ri";
 import { useNavigate } from 'react-router-dom';
 
 function Header() {
   const navigate = useNavigate();
+  const ACCESS_TOKEN = localStorage.getItem('access_token');
 
   const handleLogoButtonClick = () => {
     window.location.replace('/');
@@ -14,12 +15,26 @@ function Header() {
     navigate('/login');
   };
 
+  const handleLogoutButtonClick = () => {
+    localStorage.clear();
+    alert('로그아웃 되었습니다.');
+    navigate('/');
+  }
+
+
+  // 조건부 렌더링
+  // 로그인 안했을 시: 로그인 버튼
+  // 로그인 했을 시: 로그아웃 버튼
   return (
     <header className="header">
       <div className="header_left"></div>
       <div className="header_center" onClick={handleLogoButtonClick}>ez-ask</div>
       <div className="header_right">
-        <button className="header_login_button" onClick={handleLoginButtonClick}><RiLoginBoxLine /></button>
+      {ACCESS_TOKEN
+        ? <button className="header_logout_button" onClick={handleLogoutButtonClick}><RiLogoutBoxLine /></button>
+        : <button className="header_login_button" onClick={handleLoginButtonClick}><RiLoginBoxLine />
+        </button>
+      }
       </div>
     </header>
   );
